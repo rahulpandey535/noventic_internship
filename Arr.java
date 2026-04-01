d9c88b72329b5871675f20cb6f9b10e17a12b6b8
Abhishek <ad8551321@gmail.com>
2026-04-01 12:16:33 +0000

    updated repo

---
+++ Arr.java
@@ -0,0 +1,20 @@
+public class Arr {
+    
+    public static void main(String[]args){
+        int[] arr ={1,2,2,3,2,1,3,1,3};
+
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-1;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        for(int i:arr){
+            System.out.print(i);
+        }
+    }
+}
+
